6ed7a4e0129e80d726c8686f4eb1f7ae6c0ca3c3
clixmods <clix.mods.cod@gmail.Com>
2023-05-01 09:47:03 +0200

    UI title screen

---
+++ Assets/Scripts/UI/Menu/PlayLoopSoundUIMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using AudioAliase;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
+using UnityEngine.UI;
+
+[AddComponentMenu("#Survival Game/UI/UI Menu PlayLoop Sound alias")]
+public class PlayLoopSoundUIMenu : MonoBehaviour
+{
+    [Header("Aliases")] 
+    // private float _currentDelay;
+    // [SerializeField] private float delayBeforeStartLoop;
+    [Aliase] [SerializeField] private int MusicLoop;
+
+    private AudioPlayer _audioPlayer;
+    private void Awake()
+    {
+        GetComponent<UIMenu>().EventOnOpenMenu.AddListener(PlayMusic) ;
+        GetComponent<UIMenu>().EventOnCloseMenu.AddListener(StopMusic) ;
+    }
+
+    private void StopMusic()
+    {
+        AudioManager.StopLoopSound(ref _audioPlayer);
+    }
+
+    private void PlayMusic()
+    {
+        transform.PlayLoopSound(MusicLoop ,ref _audioPlayer);
+    }
+}
---
+++ Assets/Scripts/UI/Menu/UIMenuWatcher.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UIMenuWatcher : MonoBehaviour
+{
+    public UnityEvent OnOpenMenu;
+    public UnityEvent OnCloseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIMenu.EventMenuOpened += UIMenuOnEventMenuOpened; 
+        UIMenu.EventMenuClosed += UIMenuOnEventMenuClosed;
+    }
+
+    private void UIMenuOnEventMenuClosed()
+    {
+        OnCloseMenu?.Invoke();
+    }
+
+    private void UIMenuOnEventMenuOpened()
+    {
+        OnOpenMenu?.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
