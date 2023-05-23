1840859bf1a14dde4861a631e1982de974efe106
clixmods <clix.mods.cod@gmail.Com>
2023-05-23 23:23:17 +0200

    Fix health on gameover restart

--- Assets/Scripts/Scriptables/Value/ScriptableValueFloat.cs
+++ Assets/Scripts/Scriptables/Value/ScriptableValueFloat.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Data/Value/float")]
 public class ScriptableValueFloat : ScriptableValue<float>
 {
+    [SerializeField] private float _maxValue;
+    [Header("Default")]
     [SerializeField] protected float defaultValue = 0;
     [SerializeField] protected float defaultMaxValue = 0;
-    
-    
-    
-    
-    
-    [SerializeField] private float _maxValue;
+    #region Properties
+
     public override float Value { get => base.Value; set => base.Value = value > _maxValue ? _maxValue : value; }
     public float DefaultValue => defaultValue;
     public float DefaultMaxValue => defaultMaxValue;
@@ -20,5 +18,13 @@ public class ScriptableValueFloat : ScriptableValue<float>
     {
         get => _maxValue;
         set => _maxValue = value;
-    } 
+    }
+
+    #endregion
+    #region Methods
+    public void SetValueToMaxValue()
+    {
+        Value = MaxValue;
+    }
+    #endregion
 }
