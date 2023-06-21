77140598d8ba6f2b8e3f72996d241d1c63b01609
jeremyferreira <Jeremyblazerpro@gmail.com>
2023-06-21 12:07:06 +0200

    feedbackbalance

--- Assets/BalanceBehaviour.cs
+++ Assets/BalanceBehaviour.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -15,13 +16,23 @@ public class BalanceBehaviour : MonoBehaviour
     float currentRotation;
     float startRotationY;
     Rigidbody _rigidbody;
-
+    /*[SerializeField] Transform cubeWeightLeft;
+    [SerializeField] Transform cubeWeightRight;
+    float startCubeWeightPosYLeft;
+    float startCubeWeightPosYRight;
+    [SerializeField] float maxYSizeCubeFeedback = 10;*/
+    [SerializeField] TextMeshProUGUI _leftWeightText;
+    [SerializeField] TextMeshProUGUI _rightWeightText;
     float xRotation;
+
+
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         xRotation = _rigidbody.rotation.eulerAngles.x - 360;
         startRotationY = transform.rotation.eulerAngles.y;
+        /*startCubeWeightPosYLeft = cubeWeightLeft.localPosition.y;
+        startCubeWeightPosYRight = cubeWeightRight.localPosition.y;*/
     }
 
     private void Update()
@@ -34,8 +45,26 @@ public class BalanceBehaviour : MonoBehaviour
         {
             BalanceRight();
         }
+        /*cubeWeightLeft.transform.localScale = new Vector3(cubeWeightLeft.transform.localScale.x, Mathf.Lerp(0,maxYSizeCubeFeedback, (leftWeight / (leftWeight + rightWeight)))/2f, cubeWeightLeft.transform.localScale.z);
+        cubeWeightLeft.transform.localPosition = new Vector3(cubeWeightLeft.transform.localPosition.x, Mathf.Lerp(startCubeWeightPosYLeft + maxYSizeCubeFeedback/4f, startCubeWeightPosYLeft - maxYSizeCubeFeedback / 4f, (leftWeight / (leftWeight + rightWeight))), cubeWeightLeft.transform.localPosition.z);*/
     }
-
+    private void LateUpdate()
+    {
+        float currentleftweight = leftWeight;
+        _leftWeightText.text = "";
+        while (currentleftweight >=2)
+        {
+            currentleftweight -= 21;
+            _leftWeightText.text += "I";
+        }
+        float currentrightweight = rightWeight;
+        _rightWeightText.text = "";
+        while (currentrightweight >= 2)
+        {
+            currentrightweight -= 21;
+            _rightWeightText.text += "I";
+        }
+    }
 
     void BalanceRight()
     {
