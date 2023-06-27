897334dc7ea1ae6d4b1466f6703c01e33eaadc7c
clixmods <clix.mods.cod@gmail.Com>
2023-06-27 21:48:24 +0200

    fix

--- Assets/Scripts/Enemy/BossHealth.cs
+++ Assets/Scripts/Enemy/BossHealth.cs
@@ -5,33 +5,7 @@ using UnityEngine;
 using UnityEngine.AI;
 
 
-public class EnemyHealth : Character
+public class BossHealth : EnemyHealth
 {
-    [SerializeField] ScriptableValueListGameObject _allEnemyAwake;
-    [SerializeField] private bool wakeUpOnDamage = true;
-    [SerializeField] private bool destroyOnDeath = true;
-
-    public void SetWakeUpOnDamage(bool value)
-    {
-        wakeUpOnDamage = value;
-    }
-    
-    public override void Dead()
-    {
-        base.Dead();
-
-        _allEnemyAwake.RemoveUnique(this.gameObject);
-        if(destroyOnDeath)
-            Destroy(gameObject);
-        
-        
-    }
-    public override void DoDamage(float damage, Vector3 attackLocation)
-    {
-        base.DoDamage(damage, attackLocation);
-        if(wakeUpOnDamage && TryGetComponent<EnemyWakeUpBehaviour>(out EnemyWakeUpBehaviour wakeUpBehaviour))
-        {
-            wakeUpBehaviour.WakeUp();
-        }
-    }
+   
 }
